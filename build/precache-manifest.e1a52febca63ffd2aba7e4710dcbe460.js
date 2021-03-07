b5d8b88bdf0f90f532076d44f5c0c0c8a5119222
Aditya Sinha <adityasinha3201@gmail.com>
2021-03-07 16:22:35 +0530

    first commit

---
+++ build/precache-manifest.e1a52febca63ffd2aba7e4710dcbe460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92307752664965a9d230d3da45d7c631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6e34cde0e72e600eacb",
+    "url": "/static/css/main.59816f78.chunk.css"
+  },
+  {
+    "revision": "5455c4e15d5e4df034ab",
+    "url": "/static/js/2.b3b8b504.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.b3b8b504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e34cde0e72e600eacb",
+    "url": "/static/js/main.025a259f.chunk.js"
+  },
+  {
+    "revision": "135330ec4a64660687a0",
+    "url": "/static/js/runtime-main.d2780e72.js"
+  }
+]);
